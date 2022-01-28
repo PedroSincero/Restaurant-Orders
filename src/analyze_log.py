7bacb83b184abfb3303e6d951a745c4188793805
PedroSincero <dckpedro@gmail.com>
2022-01-28 17:40:46 -0300

    1/4 - finalizando maior pedido da maria

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -1,5 +1,5 @@
 import csv;
-
+from collections import Counter
 
 # def how_many_times_the_customer_asked(client, order):
 #     raise NotImplementedError
@@ -13,21 +13,27 @@ import csv;
 def csv_importer(path_to_file):
     log = {}
     with open(path_to_file) as file:
-        # log = csv.DictReader(file, ",", '"')
         reader = csv.reader(file, delimiter=",", quotechar='"')
         for row in reader:
             if row[0] not in log:
                 log[row[0]] = [[row[1], row[2]]]
-                print(log)
+                # print(log)
             else:
                 log[row[0]].append([row[1], row[2]])
     return log
 
 def analyze_log(path_to_file):
-    
+    orders_client = csv_importer(path_to_file)
+    biggest_order_Maria = most_requested_dish_by(orders_client, 'maria')
     raise NotImplementedError
 
 
-# def most_requested_dish_by(client):
-#     raise NotImplementedError
-csv_importer("data/orders_1.csv")
+def most_requested_dish_by(orders, client):
+    result = []
+    customer_orders = orders[client]
+    for i in customer_orders:
+        result.append(i[0])
+    return Counter(result).most_common(1)[0][0]
+
+result = csv_importer("data/orders_1.csv")
+most_requested_dish_by(result, 'maria')
